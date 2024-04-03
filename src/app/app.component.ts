cf32dc8754fa0b6417a3794b30a6c180598a6a24
Muhammad Yusuf <muhammadyusuf@MacBook-Air-Muhammad.local>
2024-04-03 19:58:17 +0700

    checkpoint adding jumbotron and carousel

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,17 +2,21 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MovieComponent } from './components/movie/movie.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { JumbotronComponent } from './components/jumbotron/jumbotron.component';
+import { TrandingComponent } from './components/tranding/tranding.component';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss',
-  imports: [
-    RouterOutlet,
-    MovieComponent,
-    NavbarComponent
-  ]
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss',
+    imports: [
+      RouterOutlet,
+      MovieComponent,
+      NavbarComponent,
+      JumbotronComponent,
+      TrandingComponent
+    ]
 })
 export class AppComponent {
   title = 'Test-SimpliDots';
