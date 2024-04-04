c92de6cd58c77c0a19084bf9c022a08410926252
Muhammad Yusuf <muhammadyusuf@MacBook-Air-Muhammad.local>
2024-04-04 13:00:55 +0700

    config card playlist

--- src/app/components/playlist/playlist.component.ts
+++ src/app/components/playlist/playlist.component.ts
@@ -1,17 +1,64 @@
 import { Component } from '@angular/core';
 import { CardModule } from 'primeng/card';
 import { ButtonModule } from 'primeng/button';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-playlist',
   standalone: true,
   imports: [
     CardModule,
-    ButtonModule
+    ButtonModule,
+    CommonModule
   ],
   templateUrl: './playlist.component.html',
   styleUrl: './playlist.component.scss'
 })
 export class PlaylistComponent {
+  playlists = [
+    {
+      header: 'Advance Card 1',
+      subHeader: 'Card Sub Header 2',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore sed consequuntur error repudiandae numquam deserunt quisquam repellat libero asperiores earum nam nobis, culpa ratione quam perferendis esse, cupiditate neque quas!',
+    },
+    {
+      header: 'Advance Card 2',
+      subHeader: 'Card Sub Header 2',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore sed consequuntur error repudiandae numquam deserunt quisquam repellat libero asperiores earum nam nobis, culpa ratione quam perferendis esse, cupiditate neque quas!',
+    },
+    {
+      header: 'Advance Card 3',
+      subHeader: 'Card Sub Header 3',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore sed consequuntur error repudiandae numquam deserunt quisquam repellat libero asperiores earum nam nobis, culpa ratione quam perferendis esse, cupiditate neque quas!',
+    },
+    {
+      header: 'Advance Card 4',
+      subHeader: 'Card Sub Header 4',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore sed consequuntur error repudiandae numquam deserunt quisquam repellat libero asperiores earum nam nobis, culpa ratione quam perferendis esse, cupiditate neque quas!',
+    },
+    {
+      header: 'Advance Card 5',
+      subHeader: 'Card Sub Header 5',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore sed consequuntur error repudiandae numquam deserunt quisquam repellat libero asperiores earum nam nobis, culpa ratione quam perferendis esse, cupiditate neque quas!',
+    },
+    {
+      header: 'Advance Card 6',
+      subHeader: 'Card Sub Header 6',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore sed consequuntur error repudiandae numquam deserunt quisquam repellat libero asperiores earum nam nobis, culpa ratione quam perferendis esse, cupiditate neque quas!',
+    },
+    {
+      header: 'Advance Card 7',
+      subHeader: 'Card Sub Header 7',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore sed consequuntur error repudiandae numquam deserunt quisquam repellat libero asperiores earum nam nobis, culpa ratione quam perferendis esse, cupiditate neque quas!',
+    },
+    {
+      header: 'Advance Card 8',
+      subHeader: 'Card Sub Header 8',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore sed consequuntur error repudiandae numquam deserunt quisquam repellat libero asperiores earum nam nobis, culpa ratione quam perferendis esse, cupiditate neque quas!',
+    }
+  ];
 
+  constructor () {
+    
+  }
 }
