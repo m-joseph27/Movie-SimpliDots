b481555c6a35b77acdf21b994e75c0771d312ebc
Muhammad Yusuf <muhammadyusuf@MacBook-Air-Muhammad.local>
2024-04-04 15:56:22 +0700

    fixing navigation bar

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,12 +1,84 @@
 import { Component } from '@angular/core';
+import { MenubarModule } from 'primeng/menubar';
+import { MenuItem } from 'primeng/api';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [
+    MenubarModule
+  ],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent {
+  items: MenuItem[] | undefined;
 
+  ngOnInit() {
+    this.items = [
+      {
+        label: 'Movie',
+        items: [
+          {
+            label: 'Populer',
+          },
+          {
+            separator: true
+          },
+          {
+            label: 'Sedang Diputar',
+          },
+          {
+            separator: true
+          },
+          {
+            label: 'Mendatang',
+          },
+          {
+            separator: true
+          },
+          {
+            label: 'Top Rating'
+          }
+        ]
+      },
+      {
+        label: 'Series TV',
+        icon: 'pi pi-fw pi-pencil',
+        items: [
+          {
+            label: 'Populer',
+          },
+          {
+            separator: true
+          },
+          {
+            label: 'Sedang Tayang',
+          },
+          {
+            separator: true
+          },
+          {
+            label: 'Tayang Di TV',
+          },
+          {
+            separator: true
+          },
+          {
+            label: 'Top Rating'
+          }
+        ]
+      },
+      {
+        label: 'Orang',
+        icon: 'pi pi-fw pi-user',
+        items: [
+          {
+            label: 'Orang Populer',
+            icon: 'pi pi-fw pi-user-plus'
+          },
+        ]
+      },
+    ];
+  }
 }
