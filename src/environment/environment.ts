c7fabd76eed7dda11b74cad9f33102c709c1722c
Muhammad Yusuf <muhammadyusuf@MacBook-Air-Muhammad.local>
2024-04-06 15:11:44 +0700

    adding read me file

--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: false,
   apiUrl: 'https://api.themoviedb.org/3',
+  // hard code access token because no service provide to generate token
   accessToken: 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0MWJlNDE4ZDEzMjY2NGY0MzQ2MzgzYWM4N2YwNGQ0OSIsInN1YiI6IjY2MGFiMGIzYWUzODQzMDE3ZDFkYjkyOCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.0bUnXGnZEmAG3HEadK78FHqukC5YVFRjC7FHUGKNSbk'
 };
